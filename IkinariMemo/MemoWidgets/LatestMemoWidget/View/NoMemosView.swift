032a5aff34306a2ae01eeb0624e006e90b62fc88
Masayuki Kawahima <poppinmasa@gmail.com>
2026-08-31 21:56:53 +0900

    change: NoMemosViewのテキストの文言を変更

--- IkinariMemo/MemoWidgets/LatestMemoWidget/View/NoMemosView.swift
+++ IkinariMemo/MemoWidgets/LatestMemoWidget/View/NoMemosView.swift
@@ -20,7 +20,7 @@ struct NoMemosView: View {
   // MARK: - Body
 
   var body: some View {
-    Text("メモがまだありません")
+    Text("メモがありません")
       .font(.footnote)
       .foregroundStyle(.primary.opacity(contentTextFontOpacityRate))
       .frame(maxWidth: .infinity, maxHeight: .infinity)
